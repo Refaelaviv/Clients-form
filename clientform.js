ab31bec3883bb55816a39333ad5646bb506c41e3
Refaelaviv <rafushka98@gmail.com>
2025-01-04 16:31:42 +0200

    Update clientform.js

--- clientform.js
+++ clientform.js
@@ -1,1 +1,27 @@
+import React from 'react';
 
+export default function Clientform() {
+  return (
+    <div>
+      <h1>טופס לקוח</h1>
+      <form>
+        <label>
+          שם פרטי:
+          <input type="text" name="firstName" />
+        </label>
+        <br />
+        <label>
+          שם משפחה:
+          <input type="text" name="lastName" />
+        </label>
+        <br />
+        <label>
+          טלפון:
+          <input type="tel" name="phone" />
+        </label>
+        <br />
+        <button type="submit">שלח</button>
+      </form>
+    </div>
+  );
+}
